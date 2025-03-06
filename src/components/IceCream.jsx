cbd1c2b47d4d55d1b541eb73125f05a164daac54
chenp <chenpeleg@gmail.com>
2025-03-06 18:08:55 +1100

    semi working

--- src/components/IceCream.jsx
+++ src/components/IceCream.jsx
@@ -2,7 +2,6 @@ import './IceCream.css'
 
 export const IceCream = () => {
     return (<div className="container">
-        <div className="icecream"></div>
         <div className="cone"></div>
     </div>)
 }
