5707d1e5b96eb0067d033d6fb12c26d25f9bfaa9
chenp <chenpeleg@gmail.com>
2025-03-10 14:52:55 +1100

    added css experiments

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,3 +1,5 @@
 export function AppHeader() {
-    return <h1 className=""> החנות שלי</h1>
+    return <h1 className=
+        "bg-amber-200 border-2 border-red-700 rounded p-4 font-bold"
+    > החנות שלי</h1>
 }
