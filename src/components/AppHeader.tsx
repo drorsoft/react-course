bea64e872a94b9abeb5bece49272c306d00ed62a
chenp <chenpeleg@gmail.com>
2025-03-11 16:28:42 +1100

    added cup or cone button

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,11 +1,13 @@
 export function AppHeader() {
     return (<nav className="relative flex flex-row justify-center bg-background-accent">
         <h1 className=
-            " m-0 text-small rounded p-2  font-bold"
+            " m-0  rounded p-2  font-bold"
         > גלידה מהאגדות</h1>
-        <div id="nav-buttons-container" className="text-sm absolute right-0 h-full flex flex-row">
-            <a href="#home" className="bg-amber-400 h-full flex flex-row items-center justify-center">הזמנות</a>
-            <a href="#home flex flex-row items-center justify-center h-full " >בניית גלידה</a>
+        <div id="nav-buttons-container" className="text-sm absolute right-0 h-full flex flex-row ">
+            <a href="#ice-cream-builder " className="text-xs p-3 bg-amber-400 flex flex-row items-center justify-center h-full ">בניית גלידה</a>
+
+            {/* <a href="#orders" className="p-3  h-full flex flex-row items-center justify-center">הזמנות</a> */}
+
         </div>
     </nav>)
 }
