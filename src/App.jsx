f2f6501c2684176ccfa7773108a3d89434d7c8f8
chenp <chenpeleg@gmail.com>
2025-03-11 16:35:36 +1100

    added mor flavours

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { AppHeader } from './components/AppHeader'
 import { IceCream } from './components/IceCream'
-import { TailwindExamples } from './examples/TailwindExamples'
 
 function App() {
   const [serveType, setServeType] = useState('cone') // cone | cup
@@ -19,10 +18,14 @@ function App() {
       <div className='flex flex-col items-center justify-center   h-64'>
         <IceCream serveType={serveType} />
       </div>
-      <div className="bg-background-main flex-1">
-        <button onClick={() => toggleServeType()}>
-          {serveType === 'cone' ? 'גביע' : 'כוס'}
-        </button>
+      <div className="bg-background-main flex-1 flex flex-col justify-start items-center p-4">
+        <div id='button-container' className='flex flex-col '>
+
+
+          <button className={`p-2 w-12 bg-background-accent text-black hover:ring-1 ring-purple-700 rounded`} onClick={() => toggleServeType()}>
+            {serveType === 'cone' ? 'גביע' : 'כוס'}
+          </button>
+        </div>
       </div>
     </div>
   )
