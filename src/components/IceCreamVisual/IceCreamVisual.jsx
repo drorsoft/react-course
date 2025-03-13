80a303111f891900618ef834390fc2f69dfffca2
chenp <chenpeleg@gmail.com>
2025-03-13 16:41:41 +1100

    scoop

--- src/components/IceCreamVisual/IceCreamVisual.jsx
+++ src/components/IceCreamVisual/IceCreamVisual.jsx
@@ -1,23 +1,6 @@
 import { IceCreamTaste } from '../../models/IceCreamTaste'
-import cherry from "../assets/svg/cherry.png"
-import sprinkles from "../assets/images/sprinkles.png"
 import './IceCream.css'
-import { IceCreamTopping } from '../../models/IceCreamTopping';
-
-const Toppings = ({ topping }) => {
-    switch (topping) {
-        case IceCreamTopping.Cherry:
-            return (
-                <img src={cherry} alt='cherry' className='absolute  -top-10 left-8 w-6 h-12' />)
-        case IceCreamTopping.Sprinkles:
-            return (
-                <img src={sprinkles} alt='sprinkles' className='absolute    -top-1 left-0 w-22 rounded-t-full h-12' style={{ filter: 'drop-shadow(1px 1px 1px black)' }} />)
-
-        default:
-            return null
-    }
-
-};
+import { Toppings } from './IceCreamVisualTopings'
 
 export const IceCream = ({ serveType, taste, topping }) => {
 
