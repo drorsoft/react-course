a5a67724dcf90bcf8d8e12dc3dd208a95e477e05
Chen Peleg <chenpeleg@gmail.com>
2025-03-19 19:10:54 +0200

    added lesson 04

--- src/context/globalContextProvider.tsx
+++ src/context/globalContextProvider.tsx
@@ -2,11 +2,9 @@ import { createContext, useState } from "react";
 import { GlobalContext } from "./globalContext";
 
 export const GlobalContextProvider = ({ children }) => {
-    const [isAuth, setIsAuth] = useState(true)
-
+    const [isAuth, setIsAuth] = useState(false)
 
     return <GlobalContext.Provider value={{ isAuth, setIsAuth }}>
-
         {children}
     </GlobalContext.Provider>
 }
