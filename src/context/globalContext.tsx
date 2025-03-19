a047d4b35ad2488709a62e6a659fc7948e420557
Chen Peleg <chenpeleg@gmail.com>
2025-03-19 18:36:26 +0200

    added context

---
+++ src/context/globalContext.tsx
@@ -0,0 +1,3 @@
+import { createContext } from "react";
+
+export const GlobalContext = createContext({ isAuth: false, setIsAuth: (value) => { } })
---
+++ src/context/globalContextProvider.tsx
@@ -0,0 +1,9 @@
+import { createContext, useState } from "react";
+import { GlobalContext } from "./globalContext";
+
+export const GlobalContextProvider = ({ children }) => {
+    const [isAuth, setIsAuth] = useState(true)
+    return <GlobalContext.Provider value={{ isAuth, setIsAuth }}>
+        {children}
+    </GlobalContext.Provider>
+}
