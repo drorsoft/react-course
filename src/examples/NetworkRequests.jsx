ab2baee1bcdbd660bd91ab6a5ca0db4ddcca37ab
Chen Peleg <chenpeleg@gmail.com>
2025-04-14 11:53:56 +0300

    fixed

--- src/examples/NetworkRequests.jsx
+++ src/examples/NetworkRequests.jsx
@@ -1,5 +1,16 @@
+import axios from "axios";
+import { useEffect } from "react"
 
 export const NetworkRequests = () => {
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get("https://json-placeholder.mock.beeceptor.com/posts");
+            console.log(response);
+        }
+        fetchData();
+    }, [])
+
     return (<div className="p-5 w-screen min-h-screen flex flex-col items-start justify-start  ">
 
         <div className="flex flex-row gap-4 justify-start   bg-pink-400 w-full h-44 p-2  ">
