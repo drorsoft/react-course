fe2f84ad1113dd1cce3669681e4f2b6f32afcb51
Chen Peleg <chenpeleg@gmail.com>
2025-04-14 10:43:16 +0300

    added network request component

---
+++ src/examples/NetworkRequests.jsx
@@ -0,0 +1,12 @@
+
+export const NetworkRequests = () => {
+    return (<div className="p-5 w-screen min-h-screen flex flex-col items-center justify-center  ">
+        <div className="flex flex-row gap-4 justify-start   bg-pink-400 w-full h-44 p-2  ">
+
+            <div className="w-20 h-12 border border-blue-400 p-4 bg-amber-200 flex-1/3">
+                Item 6
+            </div>
+
+        </div>
+    </div>)
+}
