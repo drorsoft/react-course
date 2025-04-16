01838b7b5d0b72cd72d0af0495c437c441005a29
Chen Peleg <chenpeleg@gmail.com>
2025-04-16 11:19:02 +0300

    changed posts

--- src/examples/NetworkRequests.jsx
+++ src/examples/NetworkRequests.jsx
@@ -1,20 +1,27 @@
 import axios from "axios";
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export const NetworkRequests = () => {
-
+    const [firstPost, setFirstPost] = useState(null);
     useEffect(() => {
         const fetchData = async () => {
             const response = await axios.get("https://json-placeholder.mock.beeceptor.com/posts");
-            console.log(response);
+            const firstPost = response.data[0];
+            setFirstPost(firstPost);
         }
         fetchData();
     }, [])
 
     return (<div className="p-5 w-screen min-h-screen flex flex-col items-start justify-start  ">
+        Network requests
+        <div dir="ltr" className="flex flex-row gap-4 justify-start    w-full h-44 p-2  ">
 
-        <div className="flex flex-row gap-4 justify-start   bg-pink-400 w-full h-44 p-2  ">
-            Network requests
+            {firstPost ? (<div>
+                <h1 className="font-bold mb-5"> {firstPost.title} </h1>
+                <div>{firstPost.body}</div>
+            </div>) : (<span>Loading...</span>)}
+
+
 
 
         </div>
