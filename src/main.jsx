f2f9d66f1231007616cc51aa018685810b4e98e3
Chen Peleg <chenpeleg@gmail.com>
2025-04-24 12:14:40 +0300

    added env files

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './App'
 
+const environmentMode = import.meta.env.MODE
+console.log(`Environment mode: ${environmentMode}`)
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
