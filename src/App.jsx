cae82aae0444bbc3f95be0378b4e0dc836064f4c
Chen Peleg <chenpeleg@gmail.com>
2025-04-24 17:35:51 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { GlobalContextProvider } from "./context/globalContextProvider"
 import { BrowserRouter, Outlet, Router, RouterProvider } from "react-router"
 import { AppRoutes } from "./router/routes"
 import { Layout } from "./layout/Layout"
-const basename = import.meta.env.BASE_URL || ''
+const basename = import.meta.env.VITE_BASE_URL || ''
 export const App = () => {
 
   return (
