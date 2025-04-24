c6e8fd3c333b1aaa3b3259a87bcb5f976be2a129
Chen Peleg <chenpeleg@gmail.com>
2025-04-24 16:56:42 +0300

    styled orders

--- src/pages/OrdersHistory.jsx
+++ src/pages/OrdersHistory.jsx
@@ -21,12 +21,24 @@ export const OrdersHistory = () => {
     return (<div className=" w-screen  h-screen overflow-hidden flex flex-col items-start justify-start  ">
         <div className="p-5 overflow-scroll w-screen  h-screen">
             הסטורית הזמנות
-            <div dir="ltr" className="flex flex-row   gap-4 justify-start w-full h-44 p-2 ">
-                {allOrdersHistory ? (<div className="flex flex-col gap-5 ">
+            <div className="flex flex-row   gap-4 justify-start w-full h-44 p-2 ">
+                {allOrdersHistory ? (<div className="flex flex-col gap-2 w-full">
                     {
-                        allOrdersHistory.map((order) => <div key={order.id}    ><div className=" w-96   rounded-md p-2 flex flex-col items-start justify-start gap-2 border border-black bg-amber-200">
-                            <h1 className="font-bold mb-5"> {order.name} - {order.email}</h1>
-                            <div>{order.address}</div>
+                        allOrdersHistory.map((order) => <div key={order.id}    ><div className=" w-full md:w-96   rounded-md p-2 flex flex-col items-start justify-start gap-2 border border-black  ">
+                            <h1 className="font-bold mb-1"> {order.name} </h1>
+                            <span className="flex flex-row flex-wrap text-xs gap-1 items-start justify-start w-full">
+                                <span>  {order.phone}</span>
+                                <span> </span>
+                                <span>   {order.email}</span>
+                                <span> </span>
+                                <span>   {order.address}</span>
+                                <span> </span>
+                                <span>{order.paymentMethod}</span>
+                                <span> </span>
+                            </span>
+                            <span className="text-xs">  </span>
+
+
                         </div></div>)
                     }
 
