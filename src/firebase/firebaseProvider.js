228312261446b0ef7d689c79091811a07b0d714a
Chen Peleg <chenpeleg@gmail.com>
2025-04-24 15:29:25 +0300

    added to local storage

--- src/firebase/firebaseProvider.js
+++ src/firebase/firebaseProvider.js
@@ -1,15 +1,27 @@
 
 import { addDoc, collection, getDocs } from "firebase/firestore";
 
-const useLocalStorage = process.env.MODE === 'production' ? true : false;
+const useLocalStorage = import.meta.env.MODE === 'production' ? true : false;
 
-
-const fakeAddDoc = async (collectionRef, data) => {
-    console.log("Fake addDoc called with data:", data);
-
-    return { id: "fakeId" };
-};
+// build a fake firebase provider for local storage
+// this is used for local development only, in production we use the real firebase provider
+const randomFirebaseId = () => {
+    return Math.random().toString(36).substring(2, 15)
+}
+const localstorageAddDoc = async (collection, data) => {
+    const allRecords = JSON.parse(localStorage.getItem(collection)) || []
+    allRecords.push(data)
+    localStorage.setItem(collection, JSON.stringify(allRecords))
+    return { id: randomFirebaseId(), ...data }
+}
+const localstorageGetDocs = async (collection) => {
+    const allRecords = JSON.parse(localStorage.getItem(collection)) || []
+    return { docs: allRecords.map((record) => ({ id: randomFirebaseId(), data: () => record })) }
+}
 
 export const firebaseProvider = () => {
+    if (useLocalStorage || true) {
+        return { addDoc: localstorageAddDoc, collection: () => { }, getDocs: localstorageGetDocs }
+    }
     return { addDoc, collection, getDocs }
 }
