4cf4ebc22e38d2e9dd8e0a473734641c56a79beb
Chen Peleg <chenpeleg@gmail.com>
2025-04-24 15:20:24 +0300

    react course lesson 4

--- src/firebase/firebaseProvider.js
+++ src/firebase/firebaseProvider.js
@@ -4,6 +4,12 @@ import { addDoc, collection, getDocs } from "firebase/firestore";
 const useLocalStorage = process.env.MODE === 'production' ? true : false;
 
 
+const fakeAddDoc = async (collectionRef, data) => {
+    console.log("Fake addDoc called with data:", data);
+
+    return { id: "fakeId" };
+};
+
 export const firebaseProvider = () => {
     return { addDoc, collection, getDocs }
 }
