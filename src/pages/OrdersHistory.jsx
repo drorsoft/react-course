ba331522cd6ed6b0c3cbb22dc67d5d876738de2b
Chen Peleg <chenpeleg@gmail.com>
2025-04-24 17:41:42 +0300

    add no orders

--- src/pages/OrdersHistory.jsx
+++ src/pages/OrdersHistory.jsx
@@ -35,31 +35,33 @@ export const OrdersHistory = () => {
         <div className="p-5 overflow-scroll w-screen  h-screen">
             הסטורית הזמנות
             <div className="flex flex-row   gap-4 justify-start w-full h-44 p-2 ">
-                {allOrdersHistory ? (<div className="flex flex-col gap-2 w-full">
-                    {
-                        allOrdersHistory.map((order) => <div key={order.id}    ><div className=" w-full md:w-96   rounded-md p-2 flex flex-col items-start justify-start gap-2 border border-black  ">
-                            <h1 className="font-bold mb-1"> {order.name} </h1>
-                            <span className="flex flex-row flex-wrap text-xs gap-1 items-start justify-start w-full">
-                                <span>  {order.phone}</span>
-                                <span> </span>
-                                <span>   {order.email}</span>
-                                <span> </span>
-                                <span>   {order.address}</span>
-                                <span> </span>
-                            </span>
-                            <span className=" text-xs">כמות פריטים בהזמנה: {order.items ? order.items.length : 0}</span>
-                            <span className="font-bold text-xs " >שיטת תשלום:
-
-                                <span className="font-bold  ">   {getPaymentMethodHebrew(order.paymentMethod)}</span>
-                            </span>
-
-
-                        </div></div>)
-                    }
-
-
-
-                </div>) : (<span>Loading...</span>)}
+                {allOrdersHistory && allOrdersHistory.length > 0 ? (
+                    <div className="flex flex-col gap-2 w-full">
+                        {allOrdersHistory.map((order) => (
+                            <div key={order.id}>
+                                <div className=" w-full md:w-96   rounded-md p-2 flex flex-col items-start justify-start gap-2 border border-black  ">
+                                    <h1 className="font-bold mb-1"> {order.name} </h1>
+                                    <span className="flex flex-row flex-wrap text-xs gap-1 items-start justify-start w-full">
+                                        <span>  {order.phone}</span>
+                                        <span> </span>
+                                        <span>   {order.email}</span>
+                                        <span> </span>
+                                        <span>   {order.address}</span>
+                                        <span> </span>
+                                    </span>
+                                    <span className=" text-xs">כמות פריטים בהזמנה: {order.items ? order.items.length : 0}</span>
+                                    <span className="font-bold text-xs " >שיטת תשלום:
+                                        <span className="font-bold  ">   {getPaymentMethodHebrew(order.paymentMethod)}</span>
+                                    </span>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                ) : allOrdersHistory && allOrdersHistory.length === 0 ? (
+                    <span>לא נמצאו הזמנות</span>
+                ) : (
+                    <span>Loading...</span>
+                )}
 
 
 
