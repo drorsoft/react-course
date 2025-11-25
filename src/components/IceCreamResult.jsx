070352d2fa57c620f759e511e6eed3ebbd26f1f6
Chen Peleg <chenpeleg@gmail.com>
2025-11-25 10:45:47 +0200

    style: comment out unused CSS styles in App.css, IceCreamResult.module.css, and index.css

--- src/components/IceCreamResult.jsx
+++ src/components/IceCreamResult.jsx
@@ -1,8 +1,14 @@
 import style from './IceCreamResult.module.css'
 
-export const IceCreamResult = ({cupOrCone,anotherProp}) => {
- 
-    return <div  className={"bg-green-300 p-2 w-2 h-2 m-1"}>
-        הגלידה שלי
-    </div>
+export const IceCreamResult=( {
+        cupOrCone, anotherProp
+    }
+
+)=> {
+
+    return <div className= {
+        "bg-green-300 p-2 w-2 h-2 m-1"
+    }
+
+    >הגלידה שלי </div>
 }
