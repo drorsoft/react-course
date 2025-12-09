3a6c7a1061233d3ab489e26d6107de0e8a045d69
Chen Peleg <chenpeleg@gmail.com>
2025-12-09 10:35:25 +0200

    refactor: clean up IceCreamResult component by removing unused code and improving readability

--- src/components/IceCreamResult/IceCreamResult.jsx
+++ src/components/IceCreamResult/IceCreamResult.jsx
@@ -1,52 +1,41 @@
-import { useEffect, useMemo } from 'react'
- 
-import './IceCreamResult.css'
-import { Toppings } from './IceResultVisualToppings'
-import { IceCreamTaste } from '../../models/IcecreamTaste'
+import { useEffect, useMemo, useState } from "react";
 
-
-
+import "./IceCreamResult.css";
+import { Toppings } from "./IceResultVisualToppings";
+import { IceCreamTaste } from "../../models/IcecreamTaste";
 
 export const IceCreamResult = ({ serveType, taste, topping }) => {
-    console.log()
+ 
 
 
-  useEffect(() => { 
-    console.log(serveType)
+  const memoToppings = useMemo(() => <Toppings topping={topping} />, [topping]);
 
-    
-  
-
-  }, [serveType]);
-
-    const memoToppings = useMemo(() => <Toppings topping={topping} />, [topping])
-
-    const scoopColor = (taste) => {
-        switch (taste) {
-            case IceCreamTaste.Chocolate:
-                return '#704b03'
-            case IceCreamTaste.Vanilla:
-                return '#fffdf7'
-            case IceCreamTaste.Strawberry:
-                return '#FA5053'
-            default:
-                return '#fffdf7'
-        }
+  const scoopColor = (taste) => {
+    switch (taste) {
+      case IceCreamTaste.Chocolate:
+        return "#704b03";
+      case IceCreamTaste.Vanilla:
+        return "#fffdf7";
+      case IceCreamTaste.Strawberry:
+        return "#FA5053";
+      default:
+        return "#fffdf7";
     }
+  };
 
-    return (
-        <div className='flex flex-col items-center justify-center'>
+  return (
+    <div className="flex flex-col items-center justify-center">
+      {taste ? (
+        <div
+          id="ice-cream-scoop"
+          className={`bg-amber-400 w-22 h-22 rounded-full relative top-8 ring-1 ring-slate-400  `}
+          style={{ backgroundColor: scoopColor(taste) }}
+        >
+          {memoToppings}
+        </div>
+      ) : null}
 
-            {taste ? (
-                <div id='ice-cream-scoop' className={`bg-amber-400 w-22 h-22 rounded-full relative top-8 ring-1 ring-slate-400  `} style={{ backgroundColor: scoopColor(taste) }}>
-                    {memoToppings}
-                </div>
-            ) : null}
-
-
-            <div className={serveType === 'cone' ? 'cone' : 'cup'}>
-
-            </div>
-        </div >
-    )
-}
+      <div className={serveType === "cone" ? "cone" : "cup"}></div>
+    </div>
+  );
+};
