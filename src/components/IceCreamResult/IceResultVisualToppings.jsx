100069a5d62060eff6a7e93522718c69808f7a2f
Chen Peleg <chenpeleg@gmail.com>
2025-12-09 08:57:03 +0200

    fix: clean up Toppings component by removing unused imports and adjusting formatting

--- src/components/IceCreamResult/IceResultVisualToppings.jsx
+++ src/components/IceCreamResult/IceResultVisualToppings.jsx
@@ -1,7 +1,6 @@
  
 import cherry from "../../assets/images/cherry.png"
-import sprinkles from "../../assets/images/sprinkles.png"
-import { useEffect } from "react";
+import sprinkles from "../../assets/images/sprinkles.png" 
 import { IceCreamTopping } from "../../models/IcecreamTopping";
 
 
@@ -12,7 +11,7 @@ export const Toppings = ({ topping }) => {
     switch (topping) {
         case IceCreamTopping.Cherry:
             return (
-                <img id={'toppings-image'} src={cherry} alt='cherry' className='absolute  -top-10 left-8 w-6 h-12' />)
+                <img id={'toppings-image'} src={cherry} alt='cherry' className='absolute -top-10 left-8 w-6 h-12' />)
         case IceCreamTopping.Sprinkles:
             return (
                 <img id={'toppings-image'} src={sprinkles} alt='sprinkles' className='absolute    -top-1 left-0 w-22 rounded-t-full h-12' style={{ filter: 'drop-shadow(1px 1px 1px black)' }} />)
