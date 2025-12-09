816b57c4dd7b40e2dfcfee3e2600911ae1cc4f1c
Chen Peleg <chenpeleg@gmail.com>
2025-12-09 08:31:57 +0200

    fix: update shadow style in AppHeader component for improved visual consistency

--- src/AppHeader.jsx
+++ src/AppHeader.jsx
@@ -2,7 +2,7 @@ export function AppHeader () {
   
     return  (
     <header 
-    className="w-screen bg-pink-300 h-16 flex flex-row justify-center items-center shadow-2xl">
+    className="w-screen bg-pink-300 h-16 flex flex-row justify-center items-center shadow-xl">
     <h1 className="text-lg font-bold">חנות הגלידות שלי</h1>
     </header>
 
