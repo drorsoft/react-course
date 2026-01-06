c3d72eb178be8c4b70626c4a22edaf05d5123a6c
Chen Peleg <chenpeleg@gmail.com>
2026-01-06 10:07:32 +0200

    feat: add Orders page and integrate with AppRouter

---
+++ src/pages/Orders.jsx
@@ -0,0 +1,10 @@
+export const Orders = () => {
+  return (
+    <div className="h-screen w-full bg-white" dir={'rtl'}>
+      <div className="flex flex-col bg-amber-300 h-1/2 w-full">
+        <h1 className="text-black">ההזמנות שלי</h1>
+      </div>
+      Orders
+    </div>
+  );
+};
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,14 @@
 import { Route } from "react-router";
 import { BrowserRouter, Routes } from "react-router";
 import { App } from "../App";
+import { Orders } from "../pages/Orders";
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/orders" element={<Orders />} />
         <Route path="try" element={<div>This is my try route</div>} />
       </Routes>
     </BrowserRouter>
