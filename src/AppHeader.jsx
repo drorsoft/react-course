bbae5bc867a0b2a7e0e38fbf65d54a816e141cc8
Chen Peleg <chenpeleg@gmail.com>
2026-01-13 07:58:57 +0200

    fix: adjust padding in AppHeader and clean up Orders component

--- src/AppHeader.jsx
+++ src/AppHeader.jsx
@@ -3,12 +3,12 @@ import { NavLink } from "react-router";
 export function AppHeader() {
   return (
     <header className="w-screen bg-pink-300 h-16 flex flex-row justify-center items-center shadow-xl">
-      <div className="absolute right-0 pr-6 flex flex-row gap-6 h-16  ">
+      <div className="absolute right-0 pr-6 flex flex-row  h-16  ">
       
         <NavLink
           className={
             ({isActive})=>
-            "bg-amber-200/30   h-full justify-center items-center flex flex-col px-4 " + (isActive ? "font-bold bg-amber-200/80 " : "")
+            "bg-amber-200/30   h-full justify-center items-center flex flex-col px-8 " + (isActive ? "font-bold bg-amber-200/80 " : "")
         }
           to={"/"}
         >
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,26 +1,13 @@
 import { useParams, useSearchParams } from "react-router";
 
 export const Orders = () => {
-    const [queryParams,] = useSearchParams()
-    
-
-    const params = useParams()
-    // 
-     console.log(new Map(queryParams)  )
-    
-
-
   return (
-    <div className="h-screen w-full bg-white text-black" dir={'rtl'}>
-      <div className="flex flex-col bg-amber-300 h-1/2 w-full">
+    <div className="h-full w-full bg-white text-black" dir={"rtl"}>
+      <div className="flex flex-col bg-amber-300   w-full">
         <h1 className="">ההזמנות שלי</h1>
         אתה צופה בהזמנה מספר
-        &nbsp;
-        {
-            params.orderId
-        }
       </div>
-      Orders
+    
     </div>
   );
 };
