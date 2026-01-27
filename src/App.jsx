4a0d9765db14527e22bb7699563510a2a7cc46c4
Chen Peleg <chenpeleg@gmail.com>
2026-01-27 09:26:28 +0200

    fix: format code for better readability and consistency in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { useEffect,useRef } from "react";
+import { useEffect, useRef } from "react";
 import { AppHeader } from "./AppHeader";
 import { IcecreamBuilderPage } from "./pages/IcecreamBuilderPage";
 import { AppRouter } from "./router/AppRouter";
 import { BrowserRouter } from "react-router";
 export function App() {
- 
+  useEffect(() => {
+    fetch("http://api.open-notify.org/iss-now.json").then((response) => {
+       response.json().then((json) => {
+        console.log(json);
+      });
+    });
+  }, []);
 
   return (
-    <div   className={"bg-amber-300 transition-all duration-300  h-screen w-screen text-black text-center"}>
+    <div
+      className={
+        "bg-amber-300 transition-all duration-300  h-screen w-screen text-black text-center"
+      }
+    >
       <BrowserRouter>
-      <AppHeader></AppHeader>
-      <AppRouter/> 
+        <AppHeader></AppHeader>
+        <AppRouter />
       </BrowserRouter>
     </div>
   );
