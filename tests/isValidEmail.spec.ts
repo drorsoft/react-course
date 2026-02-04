425a6d9a44f965004461fbaa2a05c92af5cc8753
Chen Peleg <chenpeleg@gmail.com>
2026-02-04 12:38:53 +0200

    fix: correct smoke test expectation and include tests in tsconfig

---
+++ tests/isValidEmail.spec.ts
@@ -0,0 +1,22 @@
+import { expect, it, describe } from "vitest";
+import { isValidEmail } from "../src/validators/isValidEmail";
+
+describe("isValidEmail", () => {
+  it("should validate correct email format", () => {
+    const result = isValidEmail("joe@example.com");
+    expect(result).toBe(true);
+  });
+  it("should not validate incorrect email format", () => {
+    const result = isValidEmail("joeexample.com");
+    expect(result).toBe(false);
+  });
+  it("edge case", () => {
+    const result = isValidEmail(false as unknown as string);
+    expect(result).toBe(false);
+  });
+  // describe("edge cases", () => {
+  //   test("should validate correct email format", () => {
+  //     expect(1 + 2).toBe(3);
+  //   });
+  // });
+});
